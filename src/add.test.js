6372090cc013cd021b04847e953eaa8df11af27a
samaal62 <sa97482@gmail.com>
2020-11-24 12:43:52 +0200

    Big update

--- src/add.test.js
+++ src/add.test.js
@@ -1,5 +1,9 @@
-const add = require('./add');
+import add from './add.js'
 
 test('adds 6+4 to equal 10', () => {
 	expect(add(6, 4)).toBe(10);
+});
+
+test('adds 500+400 to equal 900', () => {
+	expect(add(500, 400)).toBe(900);
 });
